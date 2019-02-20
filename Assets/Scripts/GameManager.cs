6215d7ff406185ea6c107cabc97c06dd9584571a
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-02-20 17:22:55 +0100

    k

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
         return _players[playerId];
     }
 
-    private void OnGUI()
+    void OnGUI()
     {
         GUILayout.BeginArea(new Rect(200, 200, 200, 500));
         GUILayout.BeginVertical();
