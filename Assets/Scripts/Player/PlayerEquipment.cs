bc71ad3a3cc69082b2795f3940db6584ca010f37
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-02-21 11:57:28 +0100

    Fist commit

--- Assets/Scripts/Player/PlayerEquipment.cs
+++ Assets/Scripts/Player/PlayerEquipment.cs
@@ -10,11 +10,13 @@ public class PlayerEquipment : NetworkBehaviour
     [ClientRpc]
     public void RpcPlayerShooting(string connectionId)
     {
+        Debug.Log("1");
         if (connectionToClient == null)
         {
+            Debug.Log("2");
             if (connectionId != "-1") WeaponSound.Play();
         }
-        else if (connectionToClient.connectionId.ToString() != connectionId) WeaponSound.Play();
+        else if (connectionToClient.connectionId.ToString() != connectionId) { Debug.Log("3");  WeaponSound.Play(); }
 
         Debug.Log(transform.name + " is shooting");
     }
