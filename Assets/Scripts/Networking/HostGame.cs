8a8390406399878cd9ae34e3b8d01ebd751f8d31
jedrekszor <jedrekszor@wp.pl>
2019-02-22 22:04:22 +0100

    shdfgshhk

---
+++ Assets/Scripts/Networking/HostGame.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField] private uint _roomSize = 6;
+    private string _roomName;
+    private string _password;
+    private NetworkManager _networkManager;
+
+    void Start()
+    {
+        _networkManager = NetworkManager.singleton;
+        if (_networkManager.matchMaker == null)
+        {
+            _networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string name)
+    {
+        _roomName = name;
+    }
+
+    public void SetPassword(string password)
+    {
+        _password = password;
+    }
+
+    public void CreateRoom()
+    {
+        if (_roomName != "" && _roomName != null)
+        {
+            Debug.Log("Creating Room: "+_roomName+" for "+_roomSize+" players");
+            _networkManager.matchMaker.CreateMatch(_roomName, _roomSize, true, _password, "", "", 0, 0, _networkManager.OnMatchCreate);
+            GameObject.Find("Lobby").SetActive(false);
+        }
+    }
+}
