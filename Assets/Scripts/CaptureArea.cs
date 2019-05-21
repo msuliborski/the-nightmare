dd02e5964d8e6fe6b0f3da4cb44cd152ab57ff0a
jedrek <jedrekszor@wp.pl>
2019-05-21 18:59:08 +0200

    jsdkv

---
+++ Assets/Scripts/CaptureArea.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureArea : MonoBehaviour
+{
+    private bool _isCaptured = false;
+    private List<GameObject> candles = new List<GameObject>();
+    
+    public bool IsCaptured
+    {
+        get => _isCaptured;
+        set => _isCaptured = value;
+    }
+
+    void Start()
+    {
+        
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            candles.Add(transform.GetChild(0).GetChild(i).gameObject);
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entering collision 0");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("entering collision 1");
+            Debug.Log(candles.Count);
+            foreach (var candle in candles)
+            {
+                Debug.Log("entering collision 2");
+                candle.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
