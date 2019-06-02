c85dfd78ebda281fab6718fb920eb1321eeca403
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-06-02 19:46:51 +0200

    fixes

--- Assets/Scripts/PlacementController.cs
+++ Assets/Scripts/PlacementController.cs
@@ -196,6 +196,7 @@ public class PlacementController : NetworkBehaviour
     [ClientRpc]
     void RpcRegisterLocalPlayerBeingReady() 
     {
+        Debug.Log("aaaa: " + GameManager.LocalPlayer.transform.name);
         PlacementController localPlayer = GameManager.LocalPlayer.GetComponent<PlacementController>(); // dlatego musimy wyluskac local playera
         localPlayer._currentCamera = localPlayer._actionCamera;
         if (localPlayer._currentObject != null) Destroy(localPlayer._currentObject);
