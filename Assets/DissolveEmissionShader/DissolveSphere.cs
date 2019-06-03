d09106a24e428943290815d3e50ebc416537c21a
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-06-03 19:44:36 +0200

    misty fog effect

--- Assets/DissolveEmissionShader/DissolveSphere.cs
+++ Assets/DissolveEmissionShader/DissolveSphere.cs
@@ -2,27 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DissolveEnemeyBody : MonoBehaviour {
+public class DissolveSphere : MonoBehaviour {
 
     Material mat;
-    private EnemyControllerServer _enemyControllerServer;
+    
     private float _dissolveAmount = 0f;
-    public bool Enabled { get; set; }
+   
     public float DissolveAmount { get => _dissolveAmount; }
 
     private void Start() {
         mat = GetComponent<Renderer>().material;
-        Enabled = false;
-        _enemyControllerServer = GetComponentInParent<EnemyControllerServer>();
+       
     }
 
     private void Update() {
-        if (Enabled)
-        {
-           _dissolveAmount += Time.deltaTime / 4;
-            mat.SetFloat("_DissolveAmount", DissolveAmount);
-            if (_dissolveAmount >= 1)
-                Destroy(_enemyControllerServer.gameObject);
-        }
+       _dissolveAmount += Time.deltaTime / 4;
+       mat.SetFloat("_DissolveAmount", DissolveAmount);
+       
     }
 }
