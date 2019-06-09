aef7af230b47d860c2a132fee6fdcac486d51c9f
jedrek <jedrekszor@wp.pl>
2019-06-09 13:47:44 +0200

    menu

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void GoToLobby()
+    {
+        SceneManager.LoadScene("LobbyTests");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/PlayMenuVideo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PlayMenuVideo : MonoBehaviour
+{
+    private RawImage _image;
+    private VideoPlayer _player;
+    
+    void Start()
+    {
+        _image = GameObject.Find("Background").GetComponent<RawImage>();
+        _player = GetComponent<VideoPlayer>();
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        _player.Prepare();
+        WaitForSeconds wait = new WaitForSeconds(1);
+        while (!_player.isPrepared)
+        {
+            yield return wait;
+            break;
+        }
+
+        _image.texture = _player.texture;
+        _player.Play();
+    }
+}
