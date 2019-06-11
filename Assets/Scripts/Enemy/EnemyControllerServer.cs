2d9ad52de35231b1524a8e32c86cb06a489e7a68
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-06-11 22:21:49 +0200

    aa

--- Assets/Scripts/Enemy/EnemyControllerServer.cs
+++ Assets/Scripts/Enemy/EnemyControllerServer.cs
@@ -97,8 +97,7 @@ public class EnemyControllerServer : NetworkBehaviour
 
             case EnemyState.Fighting:
 
-
-                if (_damageDest != null && (_damageDest.IsDead || !_damageDest.gameObject.activeSelf))
+                if (_damageDest == null || _damageDest.IsDead || !_damageDest.gameObject.activeSelf))
                 {
                     _damageDest = null;
                     TurnOnWalking(true);
