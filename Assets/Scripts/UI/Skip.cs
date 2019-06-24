5b46828be3095830bbebd6a6c71fe6746002aaaa
jedrek <jedrekszor@wp.pl>
2019-06-24 18:18:24 +0200

    idshkjdf

--- Assets/Scripts/UI/Skip.cs
+++ Assets/Scripts/UI/Skip.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Skip : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+
+    private void Start()
+    {
+        _player.GetComponent<VideoPlayer>().loopPointReached += Kill;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Destroy(_player);
-            Destroy(gameObject);
+            Kill(_player.GetComponent<VideoPlayer>());
         }
+    }
+
+    private void Kill(VideoPlayer vp)
+    {
+        Destroy(_player);
+        Destroy(gameObject);
     }
 }
