b28e87a4166285114ed4dacd563b40dc63c8aa5c
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-06-28 02:29:34 +0200

    aef

--- Assets/Scripts/Traps/Barrel.cs
+++ Assets/Scripts/Traps/Barrel.cs
@@ -25,7 +25,7 @@ public class Barrel : NetworkBehaviour
         _collider.enabled = false;
         _renderer.enabled = false;
         transform.GetChild(0).gameObject.SetActive(true);
-        CmdExplode();
+        if (isServer) RpcExlode();//CmdExplode();
         StartCoroutine(Decay());
     }
     
